f0c5fbbb4037f6bf73181860419be420cdb2e44c
jacobworrel <jacobworrel@gmail.com>
2019-05-22 20:57:45 -0700

    do ex 2

--- client/src/components/__tests__/login.step-1.todo.js
+++ client/src/components/__tests__/login.step-1.todo.js
@@ -1,3 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Login from '../login'
 // Basic unit test
 test('calls onSubmit with the username and password when submitted', () => {
   // Arrange
@@ -5,31 +8,38 @@ test('calls onSubmit with the username and password when submitted', () => {
   // 🐨 create a jest.fn() for your submit handler
   // 🐨 render the Login component to a div
   // 💰 const div = document.createElement('div')
+
+  const fakeCredentials = {
+    username: 'username',
+    password: 'password',
+  }
+
+  const onSubmit = jest.fn()
+  const container = document.createElement('div')
+
+  ReactDOM.render(<Login onSubmit={onSubmit} />, container)
+
   //
   // 🐨 get the field nodes
   // 💰 const inputs = div.querySelectorAll('input')
   // 💰 const form = div.querySelector('form')
   // 🐨 fill in the field values
   //
+
+  const inputs = container.querySelectorAll('input')
+  const form = container.querySelector('form')
+  inputs[0].value = fakeCredentials.username
+  inputs[1].value = fakeCredentials.password
+
   // Act
   // 🐨 submit the form:
   // 💰 formNode.dispatchEvent(new window.Event('submit'))
   //
+
+  form.dispatchEvent(new window.Event('submit'))
   // Assert
   // 🐨 ensure your submit handler was called properly
-})
 
-//////// Elaboration & Feedback /////////
-// When you've finished with the exercises:
-// 1. Copy the URL below into your browser and fill out the form
-// 2. remove the `.skip` from the test below
-// 3. Change submitted from `false` to `true`
-// 4. And you're all done!
-/*
-http://ws.kcd.im/?ws=Testing&e=login.step-1&em=
-*/
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
-  expect(submitted).toBe(true)
+  expect(onSubmit).toHaveBeenCalledTimes(1)
+  expect(onSubmit).toHaveBeenCalledWith(fakeCredentials)
 })
-////////////////////////////////
